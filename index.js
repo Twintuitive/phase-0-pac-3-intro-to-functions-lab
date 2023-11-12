932a1354526e3d8265087263fa1f72164d9162b1
Jennifer Lynn Smith <jennifer@rocktownagency.com>
2023-11-12 16:31:52 -0500

    done

--- index.js
+++ index.js
@@ -1,27 +1,35 @@
-const { shout, logShout, sayHiToHeadphonedRoommate, whisper, logWhisper } = require('./root.js');
+function shout(string) {
+  return string.toUpperCase();
+}
+
+function whisper(string) {
+  return string.toLowerCase();
+}
+
+function logShout(string) {
+  console.log(string.toUpperCase());
+}
+
+function logWhisper(string) {
+  console.log(string.toLowerCase());
+}
+
 function sayHiToHeadphonedRoommate(string) {
-    if (string === string.toLowerCase()) {
-      return "I can't hear you!";
-    } else if (string === string.toUpperCase()) {
-      return "YES INDEED!";
-    } else if (string === "Let's have dinner together!") {
-      return "I would love to!";
-    }
-  }function whisper(string) {
-    return string.toLowerCase();
-  }function shout(string) {
-    return string.toUpperCase();
+  if (string.toUpperCase() === string) {
+    return 'YES INDEED!';
+  } else if (string.toLowerCase() === string) {
+    return "I can't hear you!";
+  } else if (string === "Let's have dinner together!") {
+    return 'I would love to!';
+  } else {
+    // Handle the case when string is neither all uppercase nor all lowercase, and not "Let's have dinner together!"
   }
-  function logShout(string) {
-    console.log(string.toUpperCase());
-  }
-  function logWhisper(string) {
-    console.log(string.toLowerCase());
-  }
-  module.exports = {
-    shout,
-    logShout,
-    sayHiToHeadphonedRoommate,
-    whisper,
-    logWhisper,
-  };
+}
+
+module.exports = {
+  shout,
+  whisper,
+  logShout,
+  logWhisper,
+  sayHiToHeadphonedRoommate,
+};
